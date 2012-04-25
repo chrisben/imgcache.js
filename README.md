f1446bb30dee7fa76b39061af28e5df53ea1f1d7
chrisben <christophe.benoit@gmail.com>
2012-04-25 23:36:40 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+imgcache.js
+===========
+
+Description
+-----------
+This library has for purpose to cache images using the new html5 filesystem API : if the page is viewed offline and its images are cached through this mechanism, they will be shown instead of an empty image.
+
+It is especially useful for mobile web applications using Phonegap/Cordova where normal browser cache cannot be relied upon and where offline navigation is quite common.
+
+Used with imagesloaded as shown in examples/example2.html, you can see that it can automatically:
+* store images in cache
+* replace images with cached version if fail to load
+This is the best solution I have found so far to provide easy caching of images within a phonegap web app.
+
+This library should work with Phonegap/Cordova 1.x so the supported platforms should be:
+* Android
+* BlackBerry WebWorks (OS 5.0 and higher)
+* iOS
+* Windows Phone 7 ( Mango )
+
+It has also been made to work with the original html5 File API - currently only implemented in recent Chrome versions - to speed up testing of a whole web application from the desktop.
+
+All methods are asynchronous. Use callbacks if required.
+
+Requirements
+------------
+* jQuery (any version from 1.6 should do)
+* Phonegap/Cordova *optional* : v1.x
+* [imagesloaded] (http://desandro.github.com/imagesloaded/) *optional*
+
+Release Notes
+-------------
+* v0.1 Initial release
+
+License
+-------
+Copyright 2012 (c) Christophe BENOIT
+Apache License http://www.apache.org/licenses/LICENSE-2.0
+
+High level API
+--------------
+* ImgCache.init() *initialises the local cache*
+* ImgCache.cacheFile() *writes a copy of a file into the local cache*
+* ImgCache.useCachedFile() *replaces the img src with the cached version*
+* ImgCache.useOnlineFile() *replaces back the img src with the original (online) version*
+
+* ImgCache.clear() *clears the local cache folder*
+
+Options
+-------
+See ImgCache.options at the top of the source file for the list of options.
+Options can be overriden from your own script, no need to modify the library!
+
+Code samples
+------------
+See html files in the examples/ folder.
+
+Notes
+-----
+Used with Phonegap/Cordova:
+* Requires the File API permission in config.xml:
+```xml
+<feature name="http://api.phonegap.com/1.0/file"/>
+```
+* ImgCache.init() to be called onDeviceReady, not before!
+* [Known issue] (https://issues.apache.org/jira/browse/CB-539) with FileTransfer.download in Cordova 1.5+ 1.6x .. waiting for 1.7 for real tests..
+
+Used with Chrome (v > 12 I believe..) or future browsers that support the html5 filesystem API:
+* Beware of cross domain ajax issue! retrieve image from the same domain or set CORS solutions with the server..
+* If page opened locally (file:// ..), chrome needs to be loaded with the following flags:
+```
+--allow-file-access-from-files --allow-file-access
+```
+otherwise the local filesystem will not be accessible (security error)
+* To navigate through the local filesystem open a new tab with filesystem:http://<sitedomain>/persistent/
+
+Todo
+----
+* Automated tests
+* Local files should be stored uniquely depending on their original full path: currently if two files have the same name, they will be overwritten
+* Find a solution for cache invalidation in case an image changes
---
+++ js/imgcache.js
@@ -0,0 +1,320 @@
+/*! imgcache.js v0.1 
+   Copyright 2012 Christophe BENOIT
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+var ImgCache = {
+	// options to override before using the library (but after loading this script!)
+	options: {
+		debug: false,			/* write to console? */
+		localCacheFolder: 'imgcache',	/* name of the cache folder */
+		useDataURI: false,		/* use src="data:.."? other will use src="filesystem:.." */
+		chromeQuota: 10*1024*1024,	/* allocated cache space : here 10Mb */
+		usePersistentCache: true
+	}
+};
+
+(function($) {
+
+	var old_src_attr = 'data-old-src';
+
+	/** Helpers *****************************************************************/
+	// with a little help from http://code.google.com/p/js-uri/
+	var URI = function(str) {
+	    if (!str) str = "";
+	    // Based on the regex in RFC2396 Appendix B.
+	    var parser = /^(?:([^:\/?\#]+):)?(?:\/\/([^\/?\#]*))?([^?\#]*)(?:\?([^\#]*))?(?:\#(.*))?/;
+	    var result = str.match(parser);
+	    this.scheme    = result[1] || null;
+	    this.authority = result[2] || null;
+	    this.path      = result[3] || null;
+	    this.query     = result[4] || null;
+	    this.fragment  = result[5] || null;
+	};
+
+	var is_cordova = function() {
+		return (typeof(cordova) !== 'undefined' || typeof(phonegap) !== 'undefined');
+	};
+
+	// returns lower cased filename from full URI
+	var URIGetFileName = function(fullpath) {
+		if (!fullpath)
+			return;
+		//TODO: there must be a better way here.. (url encoded strings fail)
+		var idx = fullpath.lastIndexOf("/");
+		if (!idx)
+			return;
+		return fullpath.substr(idx + 1).toLowerCase();
+	};
+
+	// returns lower cased path from full URI
+	var URIGetPath = function(str) {
+		if (!str)
+			return;
+		var uri = URI(str);
+		return uri.path.toLowerCase();
+	};
+	/****************************************************************************/
+
+	// if no local_root set, set relative path
+	var _getCachedFilePath = function(img_src, local_root) {
+		var filename = URIGetFileName(img_src);
+		//TODO: we should here return the file path with subfolders matching its url to avoid problem when two files have same name
+		// 	for now not done because that would mean be sure that the subfolders are created (recursively) before writing the file
+		//	An example of how to recursively create subfolders:
+		// 	http://www.html5rocks.com/en/tutorials/file/filesystem/
+		// var path = URIGetPath(img_src);
+		//return (local_root ? local_root + '/' : '') + path + '/' + filename;
+		return (local_root ? local_root + '/' : '') + filename;
+	};
+
+	var _setNewImgPath = function($img, new_src, old_src) {
+		$img.attr('src', new_src);
+		// store previous url in case we need to reload it
+		$img.attr(old_src_attr, old_src);
+	};
+
+	var _createCacheDir = function(callback) {
+		if (!ImgCache.filesystem)
+			return;
+
+		var _fail = function(error) {
+			if (ImgCache.options.debug) console.log('Failed to get/create local cache directory: ' + error.code);
+		};
+		var _getDirSuccess = function(dirEntry) {
+			ImgCache.dirEntry = dirEntry;
+			if (ImgCache.options.debug) console.log('Local cache folder opened: ' + dirEntry.fullPath);
+			if (callback) callback();
+		};
+		ImgCache.filesystem.root.getDirectory(ImgCache.options.localCacheFolder, {create: true, exclusive: false}, _getDirSuccess, _fail);	
+	};
+
+	// This is a wrapper for phonegap's FileTransfer object in order to implement the same feature
+	// in Chrome (and possibly extra browsers in the future)
+	var FileTransferWrapper = function(filesystem) {
+		if (is_cordova()) {
+			// PHONEGAP
+			this.fileTransfer = new FileTransfer();
+		}
+		this.filesystem = filesystem;	// only useful for CHROME
+	};
+	FileTransferWrapper.prototype.download = function(uri, localPath, success_callback, error_callback) {
+		// PHONEGAP
+		if (this.fileTransfer) return this.fileTransfer.download(uri, localPath, success_callback, error_callback);
+
+		var filesystem = this.filesystem;
+
+		// CHROME - browsers
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', uri, true);
+		xhr.responseType = 'arraybuffer';
+		xhr.onload = function(event){
+			if (xhr.response && (xhr.status == 200 || xhr.status == 0)) {
+				filesystem.root.getFile(localPath, { create:true }, function(fileEntry) {
+					fileEntry.createWriter(function(writer){
+
+						writer.onerror = error_callback;
+						writer.onwriteend = function() { success_callback(fileEntry);  };
+
+						var BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder;
+						var bb = new BlobBuilder();
+						bb.append(xhr.response);
+						var mime_type = xhr.getResponseHeader('Content-type');
+						writer.write(bb.getBlob(mime_type), error_callback);
+
+					}, error_callback);
+				}, error_callback);
+			} else {
+				//TODO: error_callback(error)
+				if (ImgCache.options.debug) console.log('Image ' + uri + ' could not be downloaded - status: ' + xhr.status);
+			}
+		};
+		xhr.send();
+	};
+
+	// toURL for html5, toURI for cordova...
+	var _getFileEntryURL = function(entry) {
+		return entry.toURL ? entry.toURL() : entry.toURI();
+	}
+
+	ImgCache.init = function(init_callback) {
+		ImgCache.init_callback = init_callback;
+
+		var _gotFS = function(filesystem) {
+			if (ImgCache.options.debug) console.log('LocalFileSystem opened');
+
+			// store filesystem handle
+			ImgCache.filesystem = filesystem;
+
+			_createCacheDir(ImgCache.init_callback);
+		};
+		var _fail = function(error) {
+			if (ImgCache.options.debug) console.log('Failed to initialise LocalFileSystem ' + error.code);
+		};
+		if (is_cordova()) {
+			// PHONEGAP
+			var persistence = (ImgCache.options.usePersistentCache ? LocalFileSystem.PERSISTENT : LocalFileSystem.TEMPORARY);
+			window.requestFileSystem(persistence, 0, _gotFS, _fail);
+		} else {
+			//CHROME
+			window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+			window.storageInfo = window.storageInfo || window.webkitStorageInfo;
+			if (!window.storageInfo) {
+				if (ImgCache.options.debug) console.log('Your browser does not support the html5 File API');
+				return;
+			}
+			// request space for storage
+			var quota_size = ImgCache.options.chromeQuota;
+			var persistence = (ImgCache.options.usePersistentCache ? window.storageInfo.PERSISTENT : window.storageInfo.TEMPORARY);
+			window.storageInfo.requestQuota(
+				persistence, 
+				quota_size,
+				function() { /* success*/ window.requestFileSystem(persistence, quota_size, _gotFS, _fail);  },
+				function(error) { /* error*/ if (ImgCache.options.debug) console.log('Failed to request quota: ' + error.code) }
+			);
+		}
+	};
+
+	// this function will not check if image cached or not => will overwrite existing data
+	ImgCache.cacheFile = function(img_src) {
+
+		if (!ImgCache.filesystem || !ImgCache.dirEntry || !img_src)
+			return;
+
+		var filePath = _getCachedFilePath(img_src, ImgCache.dirEntry.fullPath);
+
+		var fileTransfer = new FileTransferWrapper(ImgCache.filesystem);
+		fileTransfer.download(
+			img_src,
+			filePath,
+			function(entry) {
+				if (ImgCache.options.debug) console.log("Download complete: " + entry.fullPath);
+			},
+			function(error) {
+				if (ImgCache.options.debug) {
+					if (error.source) console.log("Download error source " + error.source);
+					if (error.target) console.log("Download error target " + error.target);
+					console.log("Download error: code " +  error.code);
+				}
+			}
+		);
+	};
+
+	// $img: jQuery object of an <img/> element
+	ImgCache.useOnlineFile = function($img) {
+		if (!$img)
+			return;
+
+		var prev_src = $img.attr(old_src_attr);
+		if (prev_src)
+			$img.attr('src', prev_src);
+		$img.removeAttr(old_src_attr);
+	};
+
+	// $img: jQuery object of an <img/> element
+	ImgCache.useCachedFile = function($img, fail_callback) {
+
+		if (!ImgCache.filesystem || !ImgCache.dirEntry || !$img)
+			return;
+
+		var img_src = $img.attr('src');
+		var filename = URIGetFileName(img_src);
+		var filePath = _getCachedFilePath(img_src); // we need only a relative path
+
+		var _gotFileEntry = function(entry) {
+			if (ImgCache.options.useDataURI) {
+				var _win = function(file) {
+					var reader = new FileReader();
+					reader.onloadend = function(e) {
+						// prefix with : 'data:' + mime_type + ';base64;' + .. ?
+						/* var mime_type = '';
+						if (filename && filename.length > 4) {
+							//TODO: of course relying on extension is wrong.. but we trust our data here
+							var ext = filename.substr(filename.length - 4).toLowerCase();
+							if (ext == '.jpg' || ext == 'jpeg') {
+								mime_type = 'image/jpeg';
+							} else if (ext == '.png') {
+								mime_type = 'image/png';
+							} else if (ext == '.gif') {
+								mime_type = 'image/gif';
+							}
+						} */
+						var base64content = e.target.result;
+						if (!base64content) {
+							console.log('Error: file in cache ' + filename + ' is empty');
+							if (fail_callback) fail_callback();
+							return;
+						}
+						_setNewImgPath($img, base64content, img_src);
+						if (ImgCache.options.debug) {
+							console.log('File ' + filename + ' loaded from cache');
+						}
+					};
+					reader.readAsDataURL(file);
+				};
+				var _fail = function(error) {
+					if (ImgCache.options.debug) console.log("Error: Failed to read file " + error.code);
+					if (fail_callback) fail_callback();
+				};
+
+				entry.file(_win, _fail);
+			} else {
+				// using src="filesystem:" kind of url
+				var new_url = _getFileEntryURL(entry);
+				_setNewImgPath($img, new_url, img_src);
+				if (ImgCache.options.debug) console.log('File ' + filename + ' loaded from cache');
+			}
+		};
+		// if file does not exist in cache, cache it now!
+		var _fail = function(e) {
+			if (ImgCache.options.debug) console.log("File " + filename + " not in cache");
+			if (fail_callback) fail_callback();
+		};
+		ImgCache.dirEntry.getFile(filePath, { create: false }, _gotFileEntry, _fail);
+	}
+
+	// clears the cache
+	ImgCache.clearCache = function(success_callback, error_callback) {
+		if (!ImgCache.filesystem || !ImgCache.dirEntry) {
+			if (ImgCache.options.debug) console.log('ImgCache not loaded yet!');
+			return;
+		}
+
+		// delete cache dir completely
+		ImgCache.dirEntry.removeRecursively(
+			function(parent) {
+				if (ImgCache.options.debug) console.log('Local cache cleared');
+				// recreate the cache dir now
+				_createCacheDir(success_callback);
+			},
+			function(error) { 
+				if (ImgCache.options.debug) console.log('Failed to remove directory or its contents: ' + error.code);
+				if (error_callback) error_callback();
+			}
+		);
+	};
+
+	// returns the URI of the local cache folder (filesystem:)
+	// this function is more useful for the examples than for anything else..
+	ImgCache.getCacheFolderURI = function() {
+		if (!ImgCache.filesystem || !ImgCache.dirEntry) {
+			if (ImgCache.options.debug) console.log('ImgCache not loaded yet!');
+			return;
+		}
+
+		return _getFileEntryURL(ImgCache.dirEntry);
+	};
+})(jQuery);
+
+
