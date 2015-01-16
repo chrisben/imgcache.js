70581bf5b2d897874637ca0aa00b873941473210
chrisben <chrisben@users.noreply.github.com>
2015-01-16 13:56:18 +0100

    fixed version number in code

--- js/imgcache.js
+++ js/imgcache.js
@@ -18,7 +18,7 @@
 /*global console,LocalFileSystem,device,FileTransfer,define,module*/
 
 var ImgCache = {
-        version: '0.7.5',
+        version: '0.7.6',
         // options to override before using the library (but after loading this script!)
         options: {
             debug: false,							/* call the log method ? */
